e4d2239beb1e44c2eda460ee231aa647789ded31
jahidstm <jahidhasanstm@gmail.com>
2026-02-01 23:51:10 +0600

    Setup prettier configuration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test-format', function () {
+    return ["message" => "Hello World", "status" => 200];
 });
